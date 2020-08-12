f14747b1d42a4d07d5064fdea0ed98d17e3882c3
Pranit Patil <patilpranit83@gmail.com>
2020-08-12 15:10:28 +0530

    Update Assignment1_oopl.java

--- OOPlab_SEM3/Assignment1_oopl.java
+++ OOPlab_SEM3/Assignment1_oopl.java
@@ -41,3 +41,40 @@ public class Complex {
 	}
 	
 }
+//main class ComplexDemo
+package assignment_1;
+import java.util.Scanner;
+
+public class ComplexDemo {
+	public static void main(String[]args) {
+		Complex c1 = new Complex();
+		c1.display();
+		Scanner sc1= new Scanner(System.in);
+		System.out.println("Enter the real part of complex no(specify negative sign if no. is < 0): ");
+		double realno;
+		double imgno;
+		realno = sc1.nextDouble();
+		System.out.println("Enter the imaginary part of complex no(specify negative sign if no. is < 0) : ");
+		imgno = sc1.nextDouble();
+		Complex c2 = new Complex(realno,imgno);
+		c2.display();
+		Scanner sc2=new Scanner (System.in);
+		System.out.println("Enter the real part of complex no2(specify negative sign if no. is < 0): ");
+		double realno2;
+		double imgno2;
+		realno2 = sc2.nextDouble();
+		System.out.println("Enter the imaginary part of complex no2(specify negative sign if no. is < 0) : ");
+		imgno2 = sc2.nextDouble();
+		Complex c4 = new Complex(realno2,imgno2);
+		Complex c5 = new Complex();
+		c5.real = c2.real + c4.real;
+		c5.imgnry = c2.imgnry + c4.imgnry;
+		c5.display();
+		c5.real=c2.real - c4.real;
+		c5.imgnry=c2.imgnry - c4.imgnry;
+		c5.display();
+		
+	}
+
+}
+
